006cf828d75ffdcd042d1db9c18d3180634651a5
Ivan <ibarnstormer@gmail.com>
2019-03-27 12:53:42 -0400

    Added Basic Level Prompt Activity

--- app/src/main/java/com/example/dollarupmoneyskills/LevelPrompt.java
+++ app/src/main/java/com/example/dollarupmoneyskills/LevelPrompt.java
@@ -1,15 +1,13 @@
 package com.example.dollarupmoneyskills;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class LevelPrompt extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_level_prompt);
     }
 }
