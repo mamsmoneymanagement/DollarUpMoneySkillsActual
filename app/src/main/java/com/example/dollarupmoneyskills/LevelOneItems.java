47da57218513fba6a7919727e99d10c4b3b32352
Aditya Kaushik <adityakaushik314@gmail.com>
2019-03-29 00:18:20 -0400

    New commit

--- app/src/main/java/com/example/dollarupmoneyskills/LevelOneItems.java
+++ app/src/main/java/com/example/dollarupmoneyskills/LevelOneItems.java
@@ -1,7 +1,9 @@
 package com.example.dollarupmoneyskills;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LevelOneItems extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class LevelOneItems extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_one_items);
+    }
+    public void goToPrompt(View view){
+        Intent intent = new Intent(this, LevelPrompt.class);
+        startActivity(intent);
     }
 }
