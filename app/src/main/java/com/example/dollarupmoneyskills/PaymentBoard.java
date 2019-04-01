f3456ae9e8bc4d73dea8d7be713d77262b1c677e
Aditya Kaushik <adityakaushik314@gmail.com>
2019-04-01 12:08:45 -0400

    New commit

---
+++ app/src/main/java/com/example/dollarupmoneyskills/PaymentBoard.java
@@ -0,0 +1,50 @@
+package com.example.dollarupmoneyskills;
+
+public class PaymentBoard {
+    int numOnes;
+    int numFives;
+    int numTens;
+    int numTwenties;
+
+    public PaymentBoard(){
+        this.numOnes = 0;
+        this.numFives = 0;
+        this.numTens = 0;
+        this.numTwenties = 0;
+    }
+    //returns amount of money on board
+    public int getAmount(){
+        return numOnes+5*numFives+10*numTens+20*numTwenties;
+    }
+    public void addOne(){
+        this.numOnes += 1;
+    }
+
+    public void addFive(){
+        this.numFives += 1;
+    }
+
+    public void addTens(){
+        this.numTens += 1;
+    }
+
+    public void addTwenties(){
+        this.numTwenties += 1;
+    }
+
+    public int getNumOnes(){
+        return this.numOnes;
+    }
+
+    public int getNumFives(){
+        return this.numFives;
+    }
+
+    public int getNumTens(){
+        return this.numTens;
+    }
+
+    public int getNumTwenties(){
+        return this.numTwenties;
+    }
+}
