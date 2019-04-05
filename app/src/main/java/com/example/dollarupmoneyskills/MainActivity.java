681db99a9f1de1a1bb387be4be84f0801b5b8035
Aditya Kaushik <adityakaushik314@gmail.com>
2019-04-05 16:21:26 -0400

    This is the lowest base working model from now.

--- app/src/main/java/com/example/dollarupmoneyskills/MainActivity.java
+++ app/src/main/java/com/example/dollarupmoneyskills/MainActivity.java
@@ -1,13 +1,9 @@
 package com.example.dollarupmoneyskills;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.RadioGroup;
-import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
     public void goToLevelOne(View view){
         Intent intent = new Intent(this, LevelOneItems.class);
@@ -25,9 +19,5 @@ public class MainActivity extends AppCompatActivity {
     public void goToSettings(View view){
         Intent intent = new Intent(this, Settings_Menu.class);
         startActivity(intent);
-
-
     }
-
-
 }
