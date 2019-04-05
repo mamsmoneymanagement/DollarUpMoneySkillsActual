e1aeb134aacc49764ad5ce3e23e17b9e3be95962
lwagn314 <lauren.wagner7680@gmail.com>
2019-04-05 13:53:11 -0400

    woot

--- app/src/main/java/com/example/dollarupmoneyskills/MainActivity.java
+++ app/src/main/java/com/example/dollarupmoneyskills/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.dollarupmoneyskills;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
     public void goToLevelOne(View view){
         Intent intent = new Intent(this, LevelOneItems.class);
@@ -19,5 +25,9 @@ public class MainActivity extends AppCompatActivity {
     public void goToSettings(View view){
         Intent intent = new Intent(this, Settings_Menu.class);
         startActivity(intent);
+
+
     }
+
+
 }
