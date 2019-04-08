ad8796dea011d7d96515361000a9fb9e82ac4ff2
Aditya Kaushik <adityakaushik314@gmail.com>
2019-04-08 14:32:16 -0400

    This is cool!

---
+++ app/src/main/java/com/example/dollarupmoneyskills/Item.java
@@ -0,0 +1,22 @@
+package com.example.dollarupmoneyskills;
+
+public class Item {
+    private String name;
+    private double lowerPrice;
+    private double higherPrice;
+    private double price;
+    private String imageName;
+
+    public Item(String name, double lowerPrice, double higherPrice, String imageName){
+        this.name = name;
+        this.lowerPrice = lowerPrice;
+        this.higherPrice = higherPrice;
+        this.imageName = imageName;
+        this.price = genPrice();
+    }
+
+    public double genPrice(){
+        return (higherPrice-lowerPrice)*Math.random()+lowerPrice;
+    }
+
+}
